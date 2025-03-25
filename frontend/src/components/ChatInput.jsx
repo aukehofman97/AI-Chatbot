2c6d886402d9e8dfec757bf1097c5d7d1fa6e5b9
ruizguille <guillermo@codeawake.com>
2025-03-25 11:13:21 +0100

    Package updates & chat input fix in mobile

--- frontend/src/components/ChatInput.jsx
+++ frontend/src/components/ChatInput.jsx
@@ -12,7 +12,7 @@ function ChatInput({ newMessage, isLoading, setNewMessage, submitNewMessage }) {
   }
   
   return(
-    <div className='sticky bottom-0 bg-white py-4'>
+    <div className='sticky bottom-0 shrink-0 bg-white py-4'>
       <div className='p-1.5 bg-primary-blue/35 rounded-3xl z-50 font-mono origin-bottom animate-chat duration-400'>
         <div className='pr-0.5 bg-white relative shrink-0 rounded-3xl overflow-hidden ring-primary-blue ring-1 focus-within:ring-2 transition-all'>
           <textarea
